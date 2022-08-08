5e1b5f0f8bec94072f49dbf73024076e1aec8494
aniketpandey8989 <pandeyaniket546@gmail.com>
2022-08-08 12:20:15 +0530

    delete unused files

--- src/pages/productionPlaning.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react'
-const ProductionPlaning = () => {
-  return (
-    <div><ProductionPlaning/></div>
-  )
-}
-
-export default ProductionPlaning
--- src/pages/productionplaning.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const productionplaning = () => {
-  return (
-    <div>production planing</div>
-  )
-}
-
-export default productionplaning
